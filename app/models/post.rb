36749ef181ace172ef5566f25ee7de4a27839de1
Ariel Feingold <feingold.ariel@gmail.com>
2018-04-19 23:27:24 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  validates :name, presence: true
+  validates :title, presence: true
   validates :content, length: { minimum: 10 }
   validates :size, inclusion: { in: %w(Fiction Non-Fiction) }
 end
